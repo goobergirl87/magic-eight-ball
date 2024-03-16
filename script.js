6afd800aa102121379e05cbe271bde54cdbbe4cd
goobergirl87 <isabelheidekorn@gmail.com>
2024-03-16 16:03:11 +0100

    Add random array element logic

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 "use strict";
+
+const button = document.querySelector(".roll");
+const answer = document.querySelector(".answer");
+
+const rollTheBall = function () {
+  answer.textContent =
+    prophecies[Math.floor(Math.random() * prophecies.length)];
+};
+
+// timer to show the prophecy and shaking during the timer
+
+const prophecies = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy",
+  "try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
+button.addEventListener("click", rollTheBall);
